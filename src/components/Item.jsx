16ad68947f914e6c5df496e1098e2eb10e789963
Tushar Ojha <tusharojha2001@gmail.com>
2021-12-18 16:14:57 +0530

    updated card UI

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,10 @@ import "./item.css";
 export const Item = () => {
   return (
     <div className="itemTile">
-      <h5 className="donationTitle">Team OpenSeas</h5>
+      <h5 className="donationTitle">Team OpenSeas
+        <div className="status running">Running</div>
+        {/* <div className="status completed">Completed</div> */}
+      </h5>
       <p className="donationDescription">
         Help Mr.Beast and team to make the water bodies cleaner!
       </p>
