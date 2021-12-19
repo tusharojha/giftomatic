079c998ef80f83ff9d3cbb2c60819321c795c987
Tushar Ojha <tusharojha2001@gmail.com>
2021-12-19 22:04:34 +0530

    view details dialog

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Details = () => {
-    return <div></div>;
+    return <div className="container left">
+        <h5 className="donationTitle">Team OpenSeas
+            <div className="status running">Running</div>
+            {/* <div className="status completed">Completed</div> */}
+        </h5>
+        <p className="donationDescription">
+            Help Mr.Beast and team to make the water bodies cleaner!
+        </p>
+        <a className="link" href="#">Associated Link</a>
+        <img height={400} width={400} src="" />
+    </div>;
 }
 
 export default Details;
