98ab8d1d2a862431c98d8ac939c69bd974a6f52f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-09 01:00:36 +0800

    Delete src directory

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MORALIS_APP_ID: process.env.REACT_APP_MORALIS_APP_ID || 'FVTbGIpsx4tM6rpp1opF7fZrgWVyErZsi5hGm5bW',
-    MORALIS_SERVER_URL: process.env.REACT_APP_MORALIS_SERVER_URL || 'https://6kf86q1x2pex.usemoralis.com:2053/server',
-    CONTRACT_ADDRESS: process.env.REACT_APP_CONTRACT_ADDRESS || '0x988Bc8539C7146E0AAc464fCE9Ec5ABC97646F0C'
-}
